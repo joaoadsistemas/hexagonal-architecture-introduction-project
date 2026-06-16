af9f719f1ddf2d9b7c3fc5bee1dfb550cd5c8644
joaoadsistemas <joaoadsistemas@gmail.com>
2026-06-16 19:00:04 -0300

    feat(customer): creation of update

---
+++ src/main/java/com/joao/hexagonal_architecture_introduction_project/application/core/useCase/UpdateCustomerUseCase.java
@@ -0,0 +1,31 @@
+package com.joao.hexagonal_architecture_introduction_project.application.core.useCase;
+
+import com.joao.hexagonal_architecture_introduction_project.application.core.domain.Address;
+import com.joao.hexagonal_architecture_introduction_project.application.core.domain.Customer;
+import com.joao.hexagonal_architecture_introduction_project.application.ports.in.FindCustomerByIdInputPort;
+import com.joao.hexagonal_architecture_introduction_project.application.ports.in.UpdateCustomerInputPort;
+import com.joao.hexagonal_architecture_introduction_project.application.ports.out.FindAddressByZipCodeOutputPort;
+import com.joao.hexagonal_architecture_introduction_project.application.ports.out.UpdateCustomerOutputPort;
+
+public class UpdateCustomerUseCase implements UpdateCustomerInputPort {
+
+    private final FindCustomerByIdInputPort findCustomerByIdInputPort;
+    private final FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort;
+    private final UpdateCustomerOutputPort updateCostumerOutputPort;
+
+    public UpdateCustomerUseCase(FindCustomerByIdInputPort findCustomerByIdInputPort,
+                                 FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort,
+                                 UpdateCustomerOutputPort updateCostumerOutputPort) {
+        this.findCustomerByIdInputPort = findCustomerByIdInputPort;
+        this.findAddressByZipCodeOutputPort = findAddressByZipCodeOutputPort;
+        this.updateCostumerOutputPort = updateCostumerOutputPort;
+    }
+
+    @Override
+    public void update(Customer customer, String zipCode) {
+        findCustomerByIdInputPort.find(customer.getId());
+        Address address = findAddressByZipCodeOutputPort.find(zipCode);
+        customer.setAddress(address);
+        updateCostumerOutputPort.update(customer);
+    }
+}
